0b58b56710f6bf9a9241b1a7a126c2e76b9c5816
Lendra <lendradx@gmail.com>
2022-05-31 12:10:12 +0700

    fixing navbar

--- views/index.jsx
+++ views/index.jsx
@@ -5,11 +5,9 @@ function Head() {
     <React.StrictMode>
       <head>
         <link rel="stylesheet" href="styles/default.css" />
-        <title>LenAPI | Home</title>
+        <title>RestAPI | Home</title>
       </head>
-      <body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-      </body>
+      <body></body>
     </React.StrictMode>
   );
 }
@@ -18,10 +16,22 @@ function Root() {
   return (
     <React.StrictMode>
       <nav className="navbar">
-        <div className="container">
-          <a href="#" className="navbar-brand">lenAPI</a>
+        <a href="#" className="navbar-brand">
+          RestAPI
+        </a>
+        <div className="navbar-links">
+          <a href="/about" className="link-item">
+            About
+          </a>
+          <a href="/docs" className="link-item">
+            Docs
+          </a>
+          <a href="https://github.com/lendradxx/rest-api" target="_blank" className="link-item">
+            Source Code
+          </a>
         </div>
       </nav>
+      <main></main>
     </React.StrictMode>
   );
 }
